820ada2d91eb2faf9ee4a7d9d4457b428b2bace5
Krishna <krishnakukreja85@gmail.com>
2024-10-08 23:27:42 +0530

    Fourth Commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { Signin } from "./pages/Signin";
 import { Blog } from "./pages/Blog";
 import { Blogs } from "./pages/Blogs";
@@ -17,6 +17,24 @@ export default function App() {
           <Route path={"/blog/:id"} element={<Blog />}></Route>
           <Route path={"/publish"} element={<Publish />}></Route>
         </Routes>
+        <div className="flex justify-center pt-10">
+          <Link to={"/signup"}>
+            <button
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:ring-none"
+            >
+              Sign up
+            </button>
+          </Link>
+          <Link to={"/signin"}>
+            <button
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:ring-none"
+            >
+              Login
+            </button>
+          </Link>
+        </div>
       </BrowserRouter>
     </>
   );
