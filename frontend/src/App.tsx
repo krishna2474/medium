1cd406aec127ec40022c7c65b8c8dcc6db0dd552
Krishna <krishnakukreja85@gmail.com>
2024-10-10 14:22:40 +0530

    Redirect to /blogs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import { Signin } from "./pages/Signin";
 import { Blog } from "./pages/Blog";
 import { Blogs } from "./pages/Blogs";
@@ -6,20 +6,28 @@ import { Signup } from "./pages/Signup";
 import { AppBar } from "./components/AppBar";
 import { Publish } from "./pages/Publish";
 import { Landing } from "./pages/Landing";
+import { useEffect } from "react";
 export default function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem("token"); // Replace 'token' with your actual token key
+    if (token) {
+      navigate("/blogs"); // Redirect to /blogs if token exists
+    } else {
+      navigate("/"); // Redirect to / if token does not exist
+    }
+  }, [navigate]);
   return (
     <>
-      <BrowserRouter>
-        <AppBar />
-        <Routes>
-          <Route path={"/"} element={<Landing />}></Route>
-          <Route path={"/signup"} element={<Signup />}></Route>
-          <Route path={"/signin"} element={<Signin />}></Route>
-          <Route path={"/blogs"} element={<Blogs />}></Route>
-          <Route path={"/blog/:id"} element={<Blog />}></Route>
-          <Route path={"/publish"} element={<Publish />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <AppBar />
+      <Routes>
+        <Route path={"/"} element={<Landing />}></Route>
+        <Route path={"/signup"} element={<Signup />}></Route>
+        <Route path={"/signin"} element={<Signin />}></Route>
+        <Route path={"/blogs"} element={<Blogs />}></Route>
+        <Route path={"/blog/:id"} element={<Blog />}></Route>
+        <Route path={"/publish"} element={<Publish />}></Route>
+      </Routes>
     </>
   );
 }
